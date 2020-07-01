1c451591d12700425f166265c34257a43c232411
Vladimir Nesterov <vova.nest.2000@yandex.ru>
2020-07-01 20:20:25 +0300

    add path recovery

--- main.cpp
+++ main.cpp
@@ -12,8 +12,9 @@ const int NumOfStorage = 20;
 
 using namespace std;
 
-void dijkstra(const int &start, const vector<vector<pair<int, long long> > > &list, vector<long long> &dist) {
-    set <pair<long long, int>> unvisited; // { dist[v], v }
+void dijkstra(const int &start, const vector<vector<pair<int, long long> > > &list, vector<long long> &dist,
+              vector<int> &prev) {
+    set<pair<long long, int>> unvisited; // { dist[v], v }
     int u = start;
     dist[u] = 0;
     unvisited.insert({0, u});
@@ -25,6 +26,7 @@ void dijkstra(const int &start, const vector<vector<pair<int, long long> > > &li
             if (dist[u] + w < dist[v]) {
                 unvisited.erase({dist[v], v});
                 dist[v] = dist[u] + w;
+                prev[v] = u;
                 unvisited.insert({dist[v], v});
             }
         }
@@ -37,21 +39,38 @@ int main() {
     ios::sync_with_stdio(false);
     int n, m;
     cin >> n >> m;
-    vector<vector<pair<int, long long> > > list(NumOfStorage);
+    vector<vector<pair<int, long long> > > list(n);
 
     for (size_t i = 0; i < m; ++i) {
         long long u, v, cost;
         cin >> u >> v >> cost;
         list[u - 1].push_back({v - 1, cost});
+        list[v - 1].push_back({u - 1, cost});
     }
 
 
     for (int i = 0; i < n; ++i) {
         vector<long long> dist(n, INF);
-        dijkstra(i, list, dist);
-        for (size_t i = 0; i < n; ++i) {
-            cout << dist[i] << " ";
+        vector<int> prev(n, -1);
+        dijkstra(i, list, dist, prev);
+        for (size_t j = 0; j < n; ++j) {
+            cout << dist[j] << " ";
+            vector<int> path;
+            int y = j;
+            while (y != -1) {
+                path.push_back(y);
+                y = prev[y];
+            }
+            reverse(path.begin(), path.end());
+            for (int k = 0; k != path.size(); ++k) {
+                cout << path[k] + 1;
+                if (k != path.size() - 1) {
+                    cout << "->";
+                }
+            }
+            cout << endl;
         }
+
         cout << endl;
     }
     return 0;
